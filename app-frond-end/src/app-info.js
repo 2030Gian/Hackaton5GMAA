3440b0ca6b730dc72b1dd62e61f0b2ba0bd36885
Gian Marco Arteaga Alvarez <gian.arteaga@utec.edu.pe>
2023-11-08 16:21:46 -0500

    Groups con personas

---
+++ app-frond-end/src/app-info.js
@@ -0,0 +1,5 @@
+const appInfo = {
+    title: 'App Frond End'
+};
+export default appInfo;
+
--- demo/src/main/java/com/example/demo/controllers/GroupController.java
+++ demo/src/main/java/com/example/demo/controllers/GroupController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,6 +21,7 @@ import com.example.demo.services.GroupService;
 
 @Controller
 @RequestMapping("/groups")
+@CrossOrigin(origins = "*")
 public class GroupController {
 
     private final GroupService groupService;
